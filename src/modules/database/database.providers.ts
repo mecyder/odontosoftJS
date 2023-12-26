b2099f43d93b7538afa25e6252f28b0c9f1fa7fa
mecyder <me_cyder@hotmail.com>
2023-12-25 21:12:27 -0400

    cambiando la cofiguracion de postgres para usarla desde enviroment

--- src/modules/database/database.providers.ts
+++ src/modules/database/database.providers.ts
@@ -1,16 +1,18 @@
+import { ConfigService } from '@nestjs/config';
 import { DataSource } from 'typeorm';
 
 export const databaseProviders = [
   {
     provide: 'DATA_SOURCE',
-    useFactory: async () => {
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => {
       const dataSource = new DataSource({
         type: 'postgres',
-        host: 'localhost',
-        port: 5432,
-        username: 'postgres',
-        password: 'postgres',
-        database: 'odontosoftDBF',
+        host: configService.get<string>('POSTGRES_HOST') ?? 'localhost',
+        port: +configService.get<string>('POSTGRES_PORT') ?? 5432,
+        username: configService.get<string>('POSTGRES_USER') ?? 'postgres',
+        password: configService.get<string>('POSTGRES_PASSWORD') ?? 'postgres',
+        database: configService.get<string>('POSTGRES_DB') ?? 'odontosoftDBF',
         entities: [__dirname + '/../**/*.entity{.ts,.js}'],
         synchronize: true,
       });

3da5753849360f1223af9c0850c9ce4081f887e2
mecyder <me_cyder@hotmail.com>
2024-02-08 22:50:18 -0400

    agregando puerto default en main.ts

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,6 @@ async function bootstrap() {
     }),
   );
 
-  
   const config = new DocumentBuilder()
     .setTitle('Odontosoft-BackEnd')
     .setDescription('Backend del sistema odontologico')
@@ -24,6 +23,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(3000);
+  await app.listen(process.env.PORT || 3000, '0.0.0.0');
 }
 bootstrap();

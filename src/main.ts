cd222a3b55c0988868857e8f7e23915e245136a0
mecyder <me_cyder@hotmail.com>
2024-02-08 23:21:51 -0400

    quitando validationPipe

--- src/main.ts
+++ src/main.ts
@@ -6,13 +6,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
-  app.useGlobalPipes(
-    new ValidationPipe({
-      transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
-    }),
-  );
+  // app.useGlobalPipes(
+  //   new ValidationPipe({
+  //     transform: true,
+  //     whitelist: true,
+  //     forbidNonWhitelisted: true,
+  //   }),
+  // );
 
   const config = new DocumentBuilder()
     .setTitle('Odontosoft-BackEnd')

5b55eb719ce3caf3a2b12b5ed20b03c984154e31
mecyder <me_cyder@hotmail.com>
2024-07-03 23:30:21 -0400

    removiendo nest-keycloak-connect

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,22 +1,12 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
-import { PrintService } from './modules/print/services/print.service';
-import { Public } from 'nest-keycloak-connect';
 
 @Controller()
 export class AppController {
-  constructor(
-    private readonly appService: AppService,
-    private printService: PrintService,
-  ) {}
+  constructor(private readonly appService: AppService) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
-  }
-  @Get('print')
-  @Public()
-  print(): void {
-    this.printService.print();
   }
 }
